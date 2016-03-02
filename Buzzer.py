a3e651876152a6b22350578b14acefe5d72045c6
Guillermo Garcia Contreras <newwilliam94@gmail.com>
2016-03-02 11:46:00 -0600

    Lcd test1

--- Buzzer.py
+++ Buzzer.py
@@ -3,6 +3,9 @@ import pyupm_buzzer as upmBuzzer
 import pyupm_grove as grove
 import mraa
 
+counter = 0
+lcdDisplay.setColor(0,0,0)
+
 # Create the button object using GPIO pin 0
 button = mraa.Gpio(2)
 
@@ -14,16 +17,21 @@ chords = [upmBuzzer.DO, upmBuzzer.RE, upmBuzzer.MI, upmBuzzer.FA,
           upmBuzzer.SOL, upmBuzzer.LA, upmBuzzer.SI, upmBuzzer.DO, 
           upmBuzzer.SI];
 
+#Crete the lcd object 
+lcdDisplay = lcd.Jhd1313m1(0, 0x3E, 0x62)
+
 
 
 # Read the input and print, waiting one second between readings
 while 1:
 	if(button.read() != 0):
+	lcdDisplay.write(counter++)
 		for chord_ind in range (0,2):
     			# play each note for one second
 			print("Boton presionado")
     			buzzer.playSound(chords[chord_ind], 1000000)
     		time.sleep(0.1)
+	
 	
 
 # Delete the buzzer object
