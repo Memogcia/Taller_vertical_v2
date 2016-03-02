ebc35f58eb15810b129b5aadcce2e43b6843e6ac
César Robles <chechare_94@hotmail.com>
2016-03-02 11:51:12 -0600

    LCD wokking?

--- Buzzer.py
+++ Buzzer.py
@@ -2,9 +2,12 @@ import time
 import pyupm_buzzer as upmBuzzer
 import pyupm_grove as grove
 import mraa
+import pyupm_i2clcd as lcd
+import sys
+import random
+
 
 counter = 0
-lcdDisplay.setColor(0,0,0)
 
 # Create the button object using GPIO pin 0
 button = mraa.Gpio(2)
@@ -19,14 +22,19 @@ chords = [upmBuzzer.DO, upmBuzzer.RE, upmBuzzer.MI, upmBuzzer.FA,
 
 #Crete the lcd object 
 lcdDisplay = lcd.Jhd1313m1(0, 0x3E, 0x62)
+lcdDisplay.setColor(0,0,0)
 
 
 
 # Read the input and print, waiting one second between readings
 while 1:
 	if(button.read() != 0):
-	lcdDisplay.write(counter++)
-		for chord_ind in range (0,2):
+	lcdDisplay.write(++counter)
+	r = random.randint(0, 255)
+	g = random.randint(0, 255)
+	b = random.randint(0, 255)
+	lcdDisplay.setColor(r,g,b)
+		for chord_ind in range (0,1):
     			# play each note for one second
 			print("Boton presionado")
     			buzzer.playSound(chords[chord_ind], 1000000)
